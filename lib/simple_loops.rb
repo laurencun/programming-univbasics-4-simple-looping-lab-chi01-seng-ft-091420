b7cbfc991f24af2c003115853f659616920d8f97
Lauren Cunningham <lauren.busbee@gmail.com>
2020-07-03 23:30:43 +0000

    Automatically backed up by Learn

--- lib/simple_loops.rb
+++ lib/simple_loops.rb
@@ -1,8 +1,8 @@
 # Write your methods here
  counter = 0
-def loop_message_five_times (counter)
+def loop_message_five_times (message)
   while counter < 5
-  puts [message]
+  puts "#{message}"
   counter += 1 
 end
 end
