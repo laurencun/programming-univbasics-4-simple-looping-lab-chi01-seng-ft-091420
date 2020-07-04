35cd4a0306df58a771c709fc868dbbc2c2223b6c
Lauren Cunningham <lauren.busbee@gmail.com>
2020-07-04 00:16:43 +0000

    Automatically backed up by Learn

--- lib/simple_loops.rb
+++ lib/simple_loops.rb
@@ -8,10 +8,10 @@ def loop_message_five_times (message)
 end
 end
 
-def loop_message_n_times (limit, message)
+def loop_message_n_times (message, limit)
   count = 0 
   while count < limit 
-    puts "#{message}"
+    puts message
     count += 1 
 end
 end
